9523fbdee1a14170eaf871edbaca4e0217a0276c
Daniel <daniloviciv@hotmail.com>
2019-01-20 20:29:14 +0100

    test using reflection vs test using assertAttributeInternalType

---
+++ php/tests/ProductTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testIDIsAnIntegerUsingReflection()
+    {
+        $product = new Product();
+
+        $reflector = new ReflectionClass(Product::class);
+
+        $property = $reflector->getProperty('product_id');
+        $property->setAccessible(true);
+
+        $value = $property->getValue($product);
+
+        $this->assertIsInt($value);
+    }
+
+    public function testIdIsAnInteger()
+    {
+        $product = new Product();
+
+        $this->assertAttributeInternalType('int', 'product_id', $product);
+
+    }
+}
